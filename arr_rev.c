1d5b93392be803e546ff7a1c2402388316f7f8ea
Likith <likithrgowda8@gmail.com>
2025-07-22 17:18:43 +0530

    funtions to reverse the array, find minimum and maximum and counting numbers

--- arr_rev.c
+++ arr_rev.c
@@ -18,9 +18,23 @@ int main(){
 
 void arr_rev(int* arr,int size){
     int temp=0;
-    for (int i=0;i<size/2;i++){
-        temp=arr[i];
-        arr[i]=arr[size-i-1];
-        arr[size-i-1]=temp;
+    //index approach
+    // for (int i=0;i<size/2;i++){
+    //     temp=arr[i];
+    //     arr[i]=arr[size-i-1];
+    //     arr[size-i-1]=temp;
+    // }
+
+    // 2 pointer approach
+    int* right=arr+size-1;
+    int*left=arr;
+    while(left<right){
+        temp=*left;
+        *left=*right;
+        *right=temp;
+        left++;
+        right--;
     }
+
+
 }
